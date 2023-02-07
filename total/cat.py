37f65289fe6cc7065e28064bd8b9769d51fc636e
easyh321 <123926357+easyh321@users.noreply.github.com>
2023-02-08 01:01:57 +0900

    cat.html : 고양이 데이터 끌어온 HTML 파일 cat.py : 고양이 데이터 연동 파이썬 파일

---
+++ total/cat.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.temtprh.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.animals
+
+@app.route('/')
+def cat():
+    return render_template('cat.html')
+
+@app.route("/total", methods=["GET"])
+def cat_get():
+    cat_list = list(db.cat.find({}, {'_id': False}))
+    return jsonify({'cats':cat_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
